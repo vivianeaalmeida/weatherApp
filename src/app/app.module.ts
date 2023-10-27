d65bdcfbca818612064ebdc7a62327a0f2ee88dc
Viviane <viviane.aalmeida@live.com>
2023-10-27 22:04:57 +0100

    scss added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
-    HomeComponent
+    MainComponent
   ],
   imports: [
     BrowserModule,
